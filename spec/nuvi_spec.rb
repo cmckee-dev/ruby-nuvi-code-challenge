4d6e67d62146b7c4b2d66efa508fd5c233ec8c69
Curtis Mckee <cmckee.dev@gmail.com>
2016-08-29 23:39:03 -0600

    Initial Commit

---
+++ spec/nuvi_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Nuvi do
+  it 'has a version number' do
+    expect(Nuvi::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
